c4fb3f0db20693966d4409554872f8eea40ae27c
Night-Voyager <18722007@bjtu.edu.cn>
2021-12-12 11:12:17 +0800

    feat: update the way of handling numeric constants

--- test.c
+++ test.c
@@ -2,7 +2,7 @@ int main() {
 	// single line comment
 	char c = '';
 	char d = '\n';
-	printf("%d", 037 + 08 + 0xbcD + 0XEfA + 1ul + 0L + 0.0 + 1. + .2 + 1e-1);
+	printf("%d", 037ul + 08 + 0xbcDL + 0XEfA + 1ul + 0L + 0.0 + 1. + .2 + 1e-1l);
 	/*
 	 * multi line comment
 	 */
