7347ac2d2e8a221166585ec5949ee9b64e2c385b
Night-Voyager <18722007@bjtu.edu.cn>
2021-12-12 10:33:25 +0800

    feat: update the way of handling decimal integers

--- test.c
+++ test.c
@@ -2,7 +2,7 @@ int main() {
 	// single line comment
 	char c = '';
 	char d = '\n';
-	printf("%d", 037 + 0xbcD + 1ul + 0L + 0.0 + 1. + .2 + 1e-1);
+	printf("%d", 037 + 08 + 0xbcD + 1ul + 0L + 0.0 + 1. + .2 + 1e-1);
 	/*
 	 * multi line comment
 	 */
